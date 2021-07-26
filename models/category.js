85bce599dff896f55598b0eb0f1c52e9f1399a93
Jackal <programming>
2021-07-26 09:57:53 -0400

    fixed some typos I think I did

--- models/category.js
+++ models/category.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var CategorySchema = new Schema({
- title: { type: string, required: true, maxLength: 100 },
- description: { type: string, required: true, maxLength: 100 }
+ title: { type: String, required: true, maxLength: 100 },
+ description: { type: String, required: true, maxLength: 100 }
 });
--- models/computerpart.js
+++ models/computerpart.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ComputerPartSchema = new Schema({
- Name: { type: string, required: true, maxLength: 100 },
- Description: { type: string, required: true, maxLength: 100 }
- Instock: {type: boolean, required: true }
- Price: { type: string, required: true, maxLength: 100 },
+ Name: { type: String, required: true, maxLength: 100 },
+ Description: { type: String, required: true, maxLength: 100 }
+ Instock: {type: Boolean, required: true }
+ Price: { type: String, required: true, maxLength: 100 },
  Category: [{type: Schema.Types.ObjectId, ref: 'Category'}]
  Manufacturer: {type: Schema.Types.ObjectId, ref: 'Manufacturer', required: true},
 });
