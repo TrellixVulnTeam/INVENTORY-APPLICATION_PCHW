2f0cf4c1c588c87f05e23b45a59b054bbce2a566
Jackal <programming>
2021-07-26 09:36:19 -0400

    removed some old code

--- routes/index.js
+++ routes/index.js
@@ -5,27 +5,6 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-/* GET Categories Page page. */
-router.get('/Categories', function(req, res, next) {
-  res.render('category_list', { title: 'Categories' });
-});
-/* GET ComputerParts List page. */
-router.get('/ComputerParts', function(req, res, next) {
-  res.render('computerparts_list', { title: 'ComputerParts List' });
-});
-router.get('/ComputerParts', function(req, res, next) {
-  res.render('computerparts_list', { title: 'ComputerParts List' });
-});
-/* GET ComputerParts Details page. */
-router.get('/ComputerParts-Detail', function(req, res, next) {
-  res.render('computerparts_Detail', { title: 'ComputerParts' });
-}); 
-router.get('/manufacturers-List', function(req, res, next) {
-  res.render('Manufacturer_list', { title: 'Manufacturer List', manufacturer_list });
-});
-router.get('/manufacturers-Detail', function(req, res, next) {
-  res.render('Manufacturer_Detail', { title: 'Manufacturer Detail' });
-});
 /*
 do the rest of the forms and pages we need in the router/index
 then work on their respctive views/page sections.
