c5b156c8c398305412bab27332f0d6ac0af0f8f3
Jackal <programming>
2021-07-28 12:00:18 -0400

    tried to fix the issue I was having

--- controllers/manufacturer.js
+++ controllers/manufacturer.js
@@ -1,18 +1,18 @@
 var Manufacturer = require('../models/manufacturer');
 var async = require("async");
 var mongoose = require("mongoose");
-
 const { body, validationResult } = require("express-validator/check");
-const { sanitizeBody, sanitize } = require("express-validator/filter");
 
+//this piece of code is causing issues to not render the view
 // Display list of all Manufacturers.
 exports.manufacturer_list = function (req, res, next) {
-  Manufacturer.find().exec(function (err, list_manufacturers) {
-    if (err) return next(err);
-
-    res.render("Manufacturer_list", {
-      title: "All Manufacturers",
-      manufacturer_list: list_manufacturers,
+  
+  Manufacturer.find()
+    .sort([['Name', 'ascending']])
+    .exec(function (err, list_manufacturers) {
+      if (err) { return next(err); }
+      //Successful, so render
+      res.render('manufacturer_list', { title: 'Author List', manufacturer_list: list_manufacturers });
     });
-  });
+
 };
