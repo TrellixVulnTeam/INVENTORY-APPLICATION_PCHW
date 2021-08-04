c1f96d236b42469834c3cbb1f8e9e86eae37dc14
Jackal <programming>
2021-08-04 10:38:40 -0400

    fixed category_list not linking to the proper detail page

--- models/category.js
+++ models/category.js
@@ -5,5 +5,7 @@ var CategorySchema = new Schema({
  title: { type: String, required: true, maxLength: 1000 },
  description: { type: String, required: true, maxLength: 1000 }
 });
-
+CategorySchema.virtual("url").get(function () {
+  return "/category/" + this._id;
+});
 module.exports = mongoose.model('Category', CategorySchema);
