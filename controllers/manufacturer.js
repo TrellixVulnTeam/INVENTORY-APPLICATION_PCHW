51ac3b7f6f91fac6f419f0419692f5f9b9a9162a
Jackal <programming>
2021-08-10 11:26:30 -0400

    more compare and contrasting to see what I got wrong

--- controllers/manufacturer.js
+++ controllers/manufacturer.js
@@ -2,8 +2,8 @@ var Manufacturer = require('../models/manufacturer');
 var async = require("async");
 var mongoose = require("mongoose");
 
-const { body, validationResult } = require("express-validator/check");
-const { sanitizeBody, sanitize } = require("express-validator/filter");
+const { body, validationResult } = require("express-validator");
+const { sanitizeBody, sanitize } = require("express-validator");
 
 // Display list of all Manufacturers.
 exports.manufacturer_list = function (req, res, next) {
@@ -25,14 +25,14 @@ exports.manufacturer_create_get = function(req, res, next) {
 }
 // Handle Manufacturer create on POST.
 exports.manufacturer_create_post = [
-  body("Name")
+  body("name")
     .trim()
     .isLength({ min: 1 })
     .withMessage("Manufacturer name must be specified"),
-  body("Description").optional({ checkFalsy: true }),
+  body("description").optional({ checkFalsy: true }),
 
-  sanitize("Name").escape(),
-  sanitize("Description").escape(),
+  sanitize("name").escape(),
+  sanitize("description").escape(),
 
   (req, res, next) => {
     const errors = validationResult(req);
@@ -47,7 +47,7 @@ exports.manufacturer_create_post = [
       return;
     } else {
       // Data from form is valid.
-      // Create a Category object with escaped and trimmed data.
+      // Create a Manufacturer object with escaped and trimmed data.
       var manufacturer = new Manufacturer({
         Name: req.body.title,
         Description: req.body.description,
